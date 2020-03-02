2c702c66502e6801e1a9795fd2639489058070ce
ksangroula <ksangroula@gmail.com>
2020-03-02 20:11:49 +0200

    more ex

---
+++ control-flow-statement/src/DiagonalStar.java
@@ -0,0 +1,26 @@
+public class DiagonalStar {
+
+
+    public static void printSquareStar (int number) {
+
+        if (number < 5) {
+            System.out.println("Invalid Value");
+        } else {
+            for (int i = 1; i <= number; i++) {
+                for (int j = 1; j <= number; j++) {
+                    if ((i == j) || (i + j == number + 1) || (i == 1) || (j == 1) || (i == number) || (j == number)) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+
+        }
+    }
+
+
+}
+
+
